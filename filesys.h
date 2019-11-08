f5ffb98907ed34b7ac7b0705a329ec5870697b75
2019202008 <indranil.pradhan@students.iiit.ac.in>
2019-11-08 19:58:31 +0530

    second commit

--- filesys.h
+++ filesys.h
@@ -1,9 +1,12 @@
+#include<bits/stdc++.h>
+
 #define block_size = 1024
 #define num_of_blocks = 20
 #define num_of_fd = 3
 #define ll long long int
 #define num_of_inodes = ceil(num_of_blocks/4)
 #define number_of_blocks_for_data = ceil(num_of_blocks / 2)
+#define num_of_direct_pointer = 2;
 
 struct superblock
 {
@@ -12,17 +15,28 @@ struct superblock
     ll num_of_blocks_for_inode_bmap =  ceil(((float)sizeof(inode_bmap)) / block_size);
 
     ll number_of_blocks_for_data_bmap = num_of_blocks - (num_of_block_for_sb + num_of_blocks_for_inode_bmap + number_of_blocks_for_data + num_of_inodes);
+
+    vector<file_inode_position> file_inode_position_map;
+
+    bool filedescriptor_bmap[num_of_fd];
 };
 
 struct inode
 {
-    char[1000] filename;
+    string filename;
     ll filesize;
-    char mode[5];
-    ll block_ptr[2];
+    ll block_ptr[num_of_direct_pointer];
+};
+
+struct file_inode_position
+{
+    ll inode;
+    ll fd;
+    string mode;
+    ll position;
 };
 
 extern bool inode_bmap[num_of_inodes];
 extern bool data_bmap[number_of_blocks_for_data];
 extern struct inode array_of_inodes[num_of_inodes];
-extern string file_descriptors[num_of_fd];
+//extern string file_descriptors[num_of_fd];
